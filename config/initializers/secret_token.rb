1845aaf953a78e3ad3116f11ae1f2d32f9df7e0e
twinks14 <svnsavani0@gmail.com>
2014-12-23 01:56:11 +0530

    Initial Commit :rocket:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChartArt::Application.config.secret_key_base = '50f323563709dd164e2cf89def50f8c6645f3974081e71d9f7ffd1a9da8c15dfe5df71b218f5820f992909361dbe22e0be149e94cf94ca60f1e5ed5fee4f5eca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChartArt::Application.config.session_store :cookie_store, key: '_chart-art_session'
